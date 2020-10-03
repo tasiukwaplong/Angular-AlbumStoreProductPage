751a53ae187125c45b65e3ee405a74afefcf714f
Tasiu Kwaplong <tasiukwaplong@gmail.com>
2020-10-03 00:27:15 -0700

    passed test 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import { Album } from './album';
+import { Product } from "./product";
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string ='../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -15,4 +17,8 @@ export class ProductService {
     <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this.._productsUrl).map((response) =>
+    <Product[]>response.json());
+  }
 }
